c5f73dc3e6083d3de6868a34e468f510af2218cd
Kirill <kirillgvozd0@gmail.com>
2023-09-07 17:55:18 +0300

    Added 1st lab to the repo

---
+++ Lab1/z03-01.bat
@@ -0,0 +1,11 @@
+::(echo off)не показываем пити>команды
+::(cls)стираем ПУТЬ>echo off
+
+echo off
+cls
+echo --current user: %username%
+echo --current date and time: %date% %time%
+echo --computer name: %computername%
+pause
+
+
